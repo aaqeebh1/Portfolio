a388284566f92030760fca8e7e682ab67b65099a
aaqeebh1 <24283167+aaqeebh1@users.noreply.github.com>
2025-04-11 04:06:50 +0100

    removed vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { injectSpeedInsights } from "@vercel/speed-insights";
 import './index.css';
-injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
